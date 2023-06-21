4ff96a730b7176fcb7b186340ea15a7275373a39
Your Name <you@example.com>
2023-06-21 18:37:39 +0100

    versioned the API url

--- ContactFormAPI/src/main/java/com/ContactFormAPI/ContactFormApiApplication.java
+++ ContactFormAPI/src/main/java/com/ContactFormAPI/ContactFormApiApplication.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@OpenAPIDefinition(info=@Info(title="ContacatForm API", version="v1.0.0", description="This a ContactForm API that sends an email notification to the owner of the site after a client has Succesfully filled in the form ."))
+@OpenAPIDefinition(info=@Info(title="ContacatForm API", version="v1.0.0", description="This a ContactForm API that sends an email notification to the owner of the site after a client has Succesfully filled in the Contact us form ."))
 public class ContactFormApiApplication {
 
 	public static void main(String[] args) {
--- ContactFormAPI/src/main/java/com/ContactFormAPI/controller/UserController.java
+++ ContactFormAPI/src/main/java/com/ContactFormAPI/controller/UserController.java
@@ -16,7 +16,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
-@RequestMapping("contents")
+@RequestMapping(value="contents")
 public class UserController {
 private final  EmailService emailService;
 
@@ -37,7 +37,7 @@ public UserController(EmailService emailService){
     content = @Content)
 
 })
-    @PostMapping("/save-content")
+    @PostMapping("/v1/save-content")
     public ResponseEntity<UserTb> addDetails(@RequestBody UserTb userTb){
         emailService.sendEmail( "hilaryemmanuel841@gmail.com", "Email", "A User has enquired");
         return Service.addUserDetails(userTb);
